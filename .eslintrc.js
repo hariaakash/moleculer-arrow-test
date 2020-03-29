c8f5b0990fbd7c94098b884f08c62b90f7fe2a37
Hari Narayanan <smgdark@gmail.com>
2020-03-29 13:31:43 +0530

    project init

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+	env: {
+		browser: true,
+		es6: true,
+		mocha: true,
+	},
+	extends: 'airbnb-base',
+	globals: {
+		Atomics: 'readonly',
+		SharedArrayBuffer: 'readonly',
+	},
+	parserOptions: {
+		ecmaVersion: 2018,
+		sourceType: 'module',
+	},
+	rules: {
+		'no-console': 0,
+		'no-param-reassign': 0,
+		indent: ['error', 'tab'],
+		'no-tabs': 0,
+		'max-len': ['error', { code: 200 }],
+		'comma-dangle': ['error', 'always-multiline'],
+		'arrow-parens': ['error', 'always'],
+		'no-underscore-dangle': 0,
+		'no-shadow': 0,
+		'no-undef': 0
+	},
+};
